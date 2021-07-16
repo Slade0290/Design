b2404f374f3d4f98bdce1ea4ea6da77a199b1239
Slade0290 <ly.david@live.fr>
2021-07-16 15:54:00 +0200

    main menu navigation ok

---
+++ functions.js
@@ -0,0 +1,85 @@
+
+function createSection() {
+	var content = document.getElementById("content")
+  content.style.gridTemplateColumns = "1fr 1fr 1fr"
+  content.style.gridTemplateRows = "1fr 1fr"
+	var arr = []
+  var titles = ["Lamp", "Invaders", "Cellular", "Unknown", "Unknown", "Unknown"]
+  for(var i = 1; i < titles.length+1; i++) {
+    arr.push(getRandomColor())
+    addSection(content, arr, titles, i)
+	}
+}
+
+function getRandomColor() {
+	return ("rgba("+getRandomInt(255) + "," +
+  				getRandomInt(255) + "," +
+          getRandomInt(255) + ", 0.5)")
+} 
+
+function addSection(content, arr, titles, i) {
+	var square = document.createElement("div")
+  square.classList.add("square")
+  square.setAttribute("id","square-"+i)
+	var section = document.createElement("section")
+  section.setAttribute("id","section-"+i)
+  section.classList.add("section")
+  section.style.background = "black"
+  var div = document.createElement("div")
+  div.onclick = function() {expendSquare(i, titles, content)};
+  var letter = titles[i-1][getRandomInt(titles.length-1)]
+  titles[i-1] = titles[i-1].replace(letter, "<span>"+letter+"</span>")
+  div.innerHTML = titles[i-1]
+  var spanClose = document.createElement("span")
+  spanClose.classList.add("close")
+  spanClose.setAttribute("id","close-"+i)
+  spanClose.onclick = function() {
+  	removeAndRebuild(i)
+  }
+  section.appendChild(spanClose)
+  section.appendChild(div)
+  square.appendChild(section)
+  setTimeout(function(){content.appendChild(square);}, i*200)
+  setTimeout(function(){section.style.background = arr[i-1];}, i*400)
+} 
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function hideSection(i, val) {
+  setTimeout(function(){
+  	val.style.opacity = 0;
+  }, i*200)
+}
+
+function wait(i, val) {
+  setTimeout(function(){
+    val.style.display = "none";
+    val.remove();
+  }, 2000)
+}
+
+
+function removeAndRebuild(i) {
+    var val = document.getElementById("square-"+i)
+    val.remove();
+    createSection();
+}
+
+function expendSquare(index, titles, content) {
+  for(let i = 1; i < titles.length+1; i++) {
+  	if(i != index) {
+    	var val = document.getElementById("square-"+i)  
+      hideSection(i, val)
+    	val.style.visibility = "hidden";
+      wait(i, val)
+    }
+  }
+  var close = document.getElementById("close-"+index)
+  setTimeout(function(){
+  	content.style.gridTemplateColumns = "1fr"
+  	content.style.gridTemplateRows = "1fr"
+  	close.style.visibility = "visible"
+  }, 1700)  
+}
